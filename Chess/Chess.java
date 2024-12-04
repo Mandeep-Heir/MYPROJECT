cb534ae49ff3c274898731f6c70651ff31f4ca99
sin12722 <sin12722@sheridancollege.ca>
2024-12-03 19:17:32 -0500

    Chess

--- Chess/Chess.java
+++ Chess/Chess.java
@@ -1,9 +1,51 @@
+import java.util.Scanner;
 
 public class Chess {
     public static void main(String[] args) {
         char[][] board = new char[8][8]; // create the 2d array
         initializeBoard(board); // set up the initial condition of the board
         printBoard(board); // print the board to display it
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printBoard(board);
+            System.out.println("Enetr the Piece,s current Position(Row and column):");
+            int currentRow = scanner.nextInt();
+            int currentCol = scanner.nextInt();
+
+            System.out.println("Enter the piece's new position(row and column):");
+            int newRow = scanner.nextInt();
+            int newCol = scanner.nextInt();
+
+            if (isMoveValid(board, currentRow, currentCol, newRow, newCol)) {
+                makeMove(board, currentRow, currentCol, newRow, newCol);
+            } else {
+                System.out.println("Invalid move! Try again.");
+            }
+
+        }
+
+    }
+
+    // isMoveValid method
+    public static boolean isMoveValid(char[][] board, int currentRow, int currentCol, int newRow, int newCol) {
+        // Validation for the move to stay within the board
+        if (currentRow < 0 || currentRow >= 8 || currentCol < 0 || currentCol >= 8 || newRow < 0 || newRow >= 8
+                || newCol < 0 || newCol >= 8) {
+            return false;
+        }
+        // Check if the piece exists at the current position
+        char piece = board[currentRow][currentCol];
+        if (piece == '\u0000') {
+            return false;
+        }
+        return true; // if the move passes the above checks then it is valid
+    }
+
+    // make the move
+
+    public static void makeMove(char[][] board, int currentRow, int currentCol, int newRow, int newCol) {
+        board[newRow][newCol] = board[currentRow][currentCol]; // move the piece to new position
+        board[currentRow][currentCol] = '\u0000'; // claer the old position
     }
 
     // Initialize the board
